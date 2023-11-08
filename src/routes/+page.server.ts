e80cbae4b95d5cb9194e8526f076051cf83a1b67
Sean Demeritt <seandemerit13@gmail.com>
2023-11-07 20:34:56 -0500

    uid

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,14 @@
-import { checkUserInteractions } from "$lib/services/sweet/sub-collections";
-import { getAllSweetDetail } from "$lib/services/sweet/sweet";
-import { SweetType } from "$lib/types/types";
-import { error } from "@sveltejs/kit";
+import { checkUserInteractions } from '$lib/services/sweet/sub-collections';
+import { getAllSweetDetail } from '$lib/services/sweet/sweet';
+import { SweetType } from '$lib/types/types';
+import { error } from '@sveltejs/kit';
 
-export const load = async ({cookies}) => {
+export const load = async ({ cookies }) => {
 	const uid = cookies.get('uid');
+	console.log('uid', uid);
 	if (!uid) throw error(401, 'User not authenticated');
 	const sweetDetailList = await getAllSweetDetail({ sweetType: SweetType.SWEET });
-	Object.assign(sweetDetailList, (await checkUserInteractions(sweetDetailList, uid)));
+	Object.assign(sweetDetailList, await checkUserInteractions(sweetDetailList, uid));
 
 	return { sweetDetailList };
 };
